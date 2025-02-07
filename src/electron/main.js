4c416a80b05ebf9fd43dfec464f9543b9eca5cc5
Bec Chao <becchao@gmail.com>
2025-02-07 22:57:09 +1100

    Initial electron setup

---
+++ src/electron/main.js
@@ -0,0 +1,9 @@
+import {app, BrowserWindow} from 'electron';
+import path from 'path';
+
+app.on('ready', () => {
+    // You can set the height and width of the winder in BrowserWindow
+    const mainWindow = new BrowserWindow({});
+    // Loading it in the right file path independent of where the user has it
+    mainWindow.loadFile(path.join(app.getAppPath() + '/dist-react/index.html'));
+})

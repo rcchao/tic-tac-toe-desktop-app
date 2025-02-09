9b2a53c39cda01b579e297afc8bf48ae14af7c14
Bec Chao <becchao@gmail.com>
2025-02-09 23:46:01 +1100

    Lock window dimensions

--- src/ui/pages/Endpage.tsx
+++ src/ui/pages/Endpage.tsx
@@ -8,7 +8,7 @@ interface GamepageProps {
 function Endpage({outcome}: GamepageProps) {
   return (
     <div>
-      {outcome !== TIE ? <h1>{outcome} wins!</h1> : <h1>It's a {outcome}!</h1>}
+      {outcome !== TIE ? <h2> Player {outcome} wins!</h2> : <h2>It's a {outcome}!</h2>}
       <button>
         <Link to="/">Play again?</Link>
       </button>

9a959e6406fcbba163e04cbb80f962bb8080aa02
Bec Chao <becchao@gmail.com>
2025-02-19 13:46:04 +1100

    Add minimise button

--- src/ui/components/TitleBar.tsx
+++ src/ui/components/TitleBar.tsx
@@ -1,12 +1,16 @@
 import "../index.css";
 import closeWindow from "../assets/closeWindow.png";
+import minimise from "../assets/minimise.png";
 
 function TitleBar() {
   return (
     <div className="title-bar-header">
       <div className="title">Tic Tac Toe</div>
-      <button onClick={() => console.log("close")}>
+      <button className="close" onClick={() => console.log("close")}>
         <img src={closeWindow} />
+      </button>
+      <button className="minimise" onClick={() => console.log("minimise")}>
+        <img src={minimise} />
       </button>
     </div>
   );
